dd47d95ed2fe63982f02905df374333e121da16c
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-04-12 22:01:47 +0200

    Updates

---
+++ precache-manifest.f059ae6c70244f7f8f3a07178eca645b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193d0537fc41ce3404ff4f3922dd47d0",
+    "url": "/ImageGallery-React-TailwindCSS/index.html"
+  },
+  {
+    "revision": "8048190ec9cede4e67bd",
+    "url": "/ImageGallery-React-TailwindCSS/static/css/main.292fa60a.chunk.css"
+  },
+  {
+    "revision": "5d3559c41905cf6029b7",
+    "url": "/ImageGallery-React-TailwindCSS/static/js/2.bab4fe11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImageGallery-React-TailwindCSS/static/js/2.bab4fe11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8048190ec9cede4e67bd",
+    "url": "/ImageGallery-React-TailwindCSS/static/js/main.91512d98.chunk.js"
+  },
+  {
+    "revision": "f6a3339247789f95d1b5",
+    "url": "/ImageGallery-React-TailwindCSS/static/js/runtime-main.3e83a827.js"
+  }
+]);
